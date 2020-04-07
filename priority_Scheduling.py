3fcf6e1ff8920cc1792024703155f23cf57ba702
Danish Ali <1712danish>
2020-04-08 03:44:54 +0530

    debugged and revised

--- priority_Scheduling.py
+++ priority_Scheduling.py
@@ -4,11 +4,12 @@ def waitingTime(processes, n, wt):
 	for i in range(1, n): 
 		wt[i] = processes[i - 1][1] + wt[i - 1] 
 
+
 def turnAroundTime(processes, n, wt, tat): 
 	
 	for i in range(n): 
-		tat[i] = processes[i][1] + wt[i]    
-
+		tat[i] = processes[i][1] + wt[i] 
+	
 def findavgTime(processes, n): 
 	wt = [0] * n 
 	tat = [0] * n 
@@ -29,21 +30,22 @@ def findavgTime(processes, n):
 				wt[i], "\t\t", tat[i]) 
 
 	print("\nAverage waiting time = %.5f "%(total_wt /n)) 
-	print("Average turn around time = ", total_tat / n)              
+	print("Average turn around time = ", total_tat / n) 
 
 def priorityScheduling(proc, n): 
 
 	proc = sorted(proc, key = lambda proc:proc[2],reverse =True); 
-    print("Order in which processes gets executed") 
-	for i in proc: 
-		print(i[0], end = " ")
-    findavgTime(proc, n)      
 
+	print("Order in which processes gets executed") 
+	for i in proc: 
+		print(i[0], end = " ") 
+	findavgTime(proc, n) 
+	
 if __name__ =="__main__": 
 	
 	process = [[1, 10, 1], 
 			[2, 20, 0], 
 			[3, 15, 1],
 			[4, 11, 2]] 
-    n = 4
-	priorityScheduling(process, n)        
+	n = 4
+	priorityScheduling(process, n)

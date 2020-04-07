84e4c42fbd73046e257c3a164cd1a902b3224fbc
Danish Ali <1712danish>
2020-04-07 23:43:40 +0530

    avgWaiting time function added

--- priority_Scheduling.py
+++ priority_Scheduling.py
@@ -7,14 +7,37 @@ def waitingTime(processes, n, wt):
 def turnAroundTime(processes, n, wt, tat): 
 	
 	for i in range(n): 
-		tat[i] = processes[i][1] + wt[i]         
+		tat[i] = processes[i][1] + wt[i]    
+
+def findavgTime(processes, n): 
+	wt = [0] * n 
+	tat = [0] * n 
+
+	waitingTime(processes, n, wt) 
+	turnAroundTime(processes, n, wt, tat) 
+
+	print("\nProcesses Burst Time Waiting", 
+		"Time Turn-Around Time") 
+	total_wt = 0
+	total_tat = 0
+	for i in range(n): 
+
+		total_wt = total_wt + wt[i] 
+		total_tat = total_tat + tat[i] 
+		print(" ", processes[i][0], "\t\t", 
+				processes[i][1], "\t\t", 
+				wt[i], "\t\t", tat[i]) 
+
+	print("\nAverage waiting time = %.5f "%(total_wt /n)) 
+	print("Average turn around time = ", total_tat / n)              
 
 def priorityScheduling(proc, n): 
 
 	proc = sorted(proc, key = lambda proc:proc[2],reverse =True); 
     print("Order in which processes gets executed") 
 	for i in proc: 
-		print(i[0], end = " ") 
+		print(i[0], end = " ")
+    findavgTime(proc, n)      
 
 if __name__ =="__main__": 
 	
